8a2d2e23af534a998cbcff54d345a39ae9fedca5
Bo Vandersteene <bo@firepeak.tech>
2022-09-26 21:20:51 +0200

    feat: create rxjs utils library

--- libs/rxjs/utils/src/lib/null-or-undefined.ts
+++ libs/rxjs/utils/src/lib/null-or-undefined.ts
@@ -5,6 +5,10 @@ export function nullOrUndefined(value) {
   return value === undefined || value === null;
 }
 
+/**
+ * Operator to check if the value is not null or undefined.
+ *
+ */
 export function isNotNullOrUndefined<T>() {
   return (o: Observable<T | null | undefined>): Observable<NonNullable<T>> =>
     o.pipe(filter(v => !nullOrUndefined(v))) as Observable<NonNullable<T>>;
