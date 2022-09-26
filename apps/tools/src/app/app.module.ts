37d25a652852e4ba9e0fcf3fb1eb87032772d1f8
Bo Vandersteene <bo@firepeak.tech>
2022-09-26 17:45:14 +0200

    feat: create rxjs utils library

--- apps/tools/src/app/app.module.ts
+++ apps/tools/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import {ResizeModule} from "@fp-tools/angular-resize";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, ResizeModule],
   providers: [],
   bootstrap: [AppComponent],
