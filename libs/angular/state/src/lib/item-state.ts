918c533cab8e97abff6f20cfa4ddd22615ced3d2
Bo-V <bo.vandersteene@sympheny.com>
2023-03-23 15:27:04 +0100

    feat: upgrade to angular 15

--- libs/angular/state/src/lib/item-state.ts
+++ libs/angular/state/src/lib/item-state.ts
@@ -1,35 +1,34 @@
-import {Injectable} from '@angular/core';
-import {AbstractState} from "./abstract-state";
-
+import { Injectable } from '@angular/core';
+import { AbstractState } from './abstract-state';
 
 @Injectable()
 export abstract class ItemState<Item, ID extends keyof Item> extends AbstractState<Item, Item[ID], Item> {
-  protected currentValue: Item | null = null;
+    protected currentValue: Item | null = null;
 
-  protected override clearCurrentValue() {
-    this.currentValue = null
-  }
+    protected override clearCurrentValue() {
+        this.currentValue = null;
+    }
 
-  protected override setCurrentValue(item: Item) {
-    this.currentValue = item
-  }
+    protected override setCurrentValue(item: Item) {
+        this.currentValue = item;
+    }
 
-  protected override updateItem(updatedItem: Item) {
-    this.updateData(updatedItem)
-    this.currentValue = updatedItem
-  }
+    protected override updateItem(updatedItem: Item) {
+        this.updateData(updatedItem);
+        this.currentValue = updatedItem;
+    }
 
-  protected override createItem(item: Item ) {
-    this.updateData(item)
-    this.currentValue = item
-  }
+    protected override createItem(item: Item) {
+        this.updateData(item);
+        this.currentValue = item;
+    }
 
-  protected override deleteItem(id: Item[ID]) {
-    this.updateData(null)
-    this.currentValue = null
-  }
+    protected override deleteItem(id: Item[ID]) {
+        this.updateData(null);
+        this.currentValue = null;
+    }
 
-  public select(key: keyof Item){
-    return this.selectData(key)
-  }
+    public select(key: keyof Item) {
+        return this.selectData(key);
+    }
 }
